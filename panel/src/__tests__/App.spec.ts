4a4e43e794ee7c27d871f8ad07e78e6322efd48f
a-maghami <Maghami.A1987@gmail.com>
2026-08-31 16:52:54 +0330

    fix

--- panel/src/__tests__/App.spec.ts
+++ panel/src/__tests__/App.spec.ts
@@ -1,11 +1,25 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
+import { createRouter, createWebHistory } from 'vue-router'
+
 import App from '../App.vue'
+import HomeView from '../views/HomeView.vue'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
-    const wrapper = mount(App)
+  it('mounts renders properly', async () => {
+    const router = createRouter({
+      history: createWebHistory(),
+      routes: [{ path: '/', component: HomeView }],
+    })
+
+    await router.push('/')
+    await router.isReady()
+
+    const wrapper = mount(App, {
+      global: { plugins: [router] },
+    })
+
     expect(wrapper.text()).toContain('You did it!')
   })
 })
